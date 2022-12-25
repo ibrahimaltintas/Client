ebbf53c820dea5ea281ff2f019a45bd629e8170c
ibrahimaltintas <110127104+ibrahimaltintas@users.noreply.github.com>
2022-12-25 18:04:56 +0300

    Color-app

---
+++ client/src/Components/Palette.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { send } from "../SocketApi";
+
+function Palette({ activeColor }) {
+  const [color, setColor] = useState("");
+  return (
+    <div className="palette">
+      <input
+        type="color"
+        value={activeColor}
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          send(color);
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+}
+
+export default Palette;
